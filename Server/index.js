ba2747ab0a1ab194414b5c9f94f8585fcb343ace
mayank gupta <mayankgupta.edu@gmail.com>
2024-01-28 19:00:00 +0530

    ES6

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors'; 
-import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import Connection from './Config/mongoose.js'; 
