e50620932c71d4135cf20b36a493146548867714
mayank gupta <mayankgupta.edu@gmail.com>
2024-01-28 20:20:08 +0530

    ES

--- Server/utils/upload.js
+++ Server/utils/upload.js
@@ -1,5 +1,5 @@
 import multer from 'multer';
-import { GridFsStorage } from 'multer-gridfs-storage';
+import multerGridFsStorage from 'multer-gridfs-storage';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,23 +7,23 @@ dotenv.config();
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
+const { GridFsStorage } = multerGridFsStorage;
+
 const storage = new GridFsStorage({
-  url: `mongodb://${username}:${password}@cluster2.vwiv9hd.mongodb.net/?retryWrites=true&w=majority`,
-  options: { useNewUrlParser: true },
-  file: (request, file) => {
-    const match = ["image/png", "image/jpg"];
+    url: `mongodb://${username}:${password}@cluster2.vwiv9hd.mongodb.net/?retryWrites=true&w=majority`,
+    options: { useNewUrlParser: true },
+    file: (request, file) => {
+        const match = ["image/png", "image/jpg"];
 
-    if (match.indexOf(file.mimetype) === -1) {
-      return `${Date.now()}-blog-${file.originalname}`;
+        if (match.indexOf(file.mimetype) === -1) {
+            return `${Date.now()}-blog-${file.originalname}`;
+        }
+
+        return {
+            bucketName: "photos",
+            filename: `${Date.now()}-blog-${file.originalname}`
+        };
     }
-
-    return {
-      bucketName: "photos",
-      filename: `${Date.now()}-blog-${file.originalname}`
-    };
-  }
 });
 
-const upload = multer({ storage });
-
-export default upload;
+export default multer({ storage });
